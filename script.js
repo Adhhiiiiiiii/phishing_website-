a8c51a9d822da7948b9502f2f2b4cf3bba02c263
ADHIYAMAN B <87760343+Adhhiiiiiiii@users.noreply.github.com>
2025-05-13 01:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function checkURL() {
 }
 
 async function checkGoogleSafe(url) {
-    const apiKey = 'YOUR_GOOGLE_API_KEY'; // Replace with your Google API Key
+    const apiKey = 'AIzaSyDa8QrFovPA9WUQ89ro1Xgzyx6xEtjHlmE'; // Replace with your Google API Key
     const endpoint = `https://safeweb.googleapis.com/v1beta1/lookup?key=${apiKey}`;
     const response = await fetch(endpoint, {
         method: 'POST',
@@ -41,7 +41,7 @@ async function checkGoogleSafe(url) {
 }
 
 async function checkVirusTotal(url) {
-    const apiKey = 'YOUR_VIRUSTOTAL_API_KEY'; // Replace with your VirusTotal API Key
+    const apiKey = 'b9d41d768ad3e995d3f772c19d3f405164b9a9a0407af7534f476fce677155c4'; // Replace with your VirusTotal API Key
     const endpoint = `https://www.virustotal.com/api/v3/urls/${btoa(url)}`;
     const response = await fetch(endpoint, {
         headers: {
